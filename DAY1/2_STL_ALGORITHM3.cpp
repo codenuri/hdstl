e06c0d4dc891c8d6c099ab07b162f7bdc4e9af82
ecourse <smkang@codenuri.co.kr>
2022-06-27 10:06:27 +0900

    Add files via upload

--- DAY1/2_STL_ALGORITHM3.cpp
+++ DAY1/2_STL_ALGORITHM3.cpp
@@ -2,9 +2,11 @@
 
 // Step 3. 검색 대상 타입의 일반화
 // => char* 대신 "템플릿 사용"
+// => 함수 이름 변경 : "strchr" -> "find"
 
+// 모든 타입의 배열에서 [first, last) 사이에서 선형검색수행
 template<typename T> 
-T* strchr(T* first, T* last, T c)
+T* find(T* first, T* last, T c)
 {
 	while (first != last && *first != c)
 		++first;
@@ -16,7 +18,7 @@ int main()
 {
 	double x[10] = { 1,2,3,4,5,6,7,8,9,10 };
 
-	double* p = strchr(x, x + 10, 5.0);
+	double* p = find(x, x + 10, 5.0);
 
 	if (p == 0)
 		std::cout << "not found" << std::endl;
