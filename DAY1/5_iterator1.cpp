715c41e7cb971f2f6fbc9c5e83a959e99283e01f
ecourse <smkang@codenuri.co.kr>
2022-06-27 13:40:23 +0900

    Add files via upload

---
+++ DAY1/5_iterator1.cpp
@@ -0,0 +1,34 @@
+// iterator1.cpp
+#include <iostream>
+#include <list>
+
+int main()
+{
+	int x[5] = { 1,2,3,4,5 };
+
+	int* p = x;
+	*p; 
+	++p;
+
+
+	std::list<int> s = { 1,2,3,4,5 };
+	std::list<int>::iterator p2 = s.begin();
+	*p2; 
+	++p2;
+
+	// 반복자의 장점
+	// 컨테이너의 내부 구조(자료구조, 메모리 모양)에
+	// 상관없이 동일한 방법으로 요소에 접근할수 있다.!!
+
+	// 디자인 패턴에서 "iterator" 패턴.!
+}
+
+
+
+
+
+
+
+
+
+
