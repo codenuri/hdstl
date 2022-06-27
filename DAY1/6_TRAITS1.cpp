f4f490fbd9e442bf26e397184679cfaf967c7f88
ecourse <smkang@codenuri.co.kr>
2022-06-27 16:13:21 +0900

    Add files via upload

---
+++ DAY1/6_TRAITS1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <list>
+
+template<typename T>
+? sum(T first, T last)
+{
+	? s = 0;
+
+	while (first != last)
+	{
+		s = s + *first;
+		++first;
+	}
+	return s;
+}
+
+int main()
+{
+	std::list<int> s = { 1,2,3,4,5,6,7,8,9,10 };
+
+	int n = sum( std::begin(s), std::end(s) );
+
+	std::cout << n << std::endl; // 55
+}
