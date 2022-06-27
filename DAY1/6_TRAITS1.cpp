db884163e8be1fd754cf73011c1b83927eb8ad65
ecourse <smkang@codenuri.co.kr>
2022-06-27 16:18:59 +0900

    Add files via upload

--- DAY1/6_TRAITS1.cpp
+++ DAY1/6_TRAITS1.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <list>
+#include <vector>
 
 template<typename T>
-? sum(T first, T last)
+typename T::value_type sum(T first, T last)
 {
-	? s = 0;
+	// value_type 은 컨테이와 반복자에 모두 있습니다.
+	// std::list<int>::value_type           : list 가 저장하는 요소의 타입
+	// std::list<int>::iterator::value_type : 반복자가 가리키는 요소의 타입
+	// 
+	// T				: 반복자 타입 ( std::list<int>::iterator )
+	// T::value_type	: 반복자가 가리키는 요소의 타입
+	typename T::value_type s = 0;
 
 	while (first != last)
 	{
@@ -16,7 +23,8 @@ template<typename T>
 
 int main()
 {
-	std::list<int> s = { 1,2,3,4,5,6,7,8,9,10 };
+//	std::list<int> s = { 1,2,3,4,5,6,7,8,9,10 };
+	std::vector<int> s = { 1,2,3,4,5,6,7,8,9,10 };
 
 	int n = sum( std::begin(s), std::end(s) );
 
