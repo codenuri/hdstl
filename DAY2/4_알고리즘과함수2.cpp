52fda21e76899b9a749dd2e8c005e9cd417488d7
ecourse <smkang@codenuri.co.kr>
2022-06-28 15:23:28 +0900

    Add files via upload

---
+++ DAY2/4_알고리즘과함수2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include "show.h"
+
+int main()
+{
+    std::list<int> s1 = { 1,2,3,4,5};
+    std::list<int> s2 = { 6,7,8,9,10 };
+    std::list<int> s3;
+
+    // transform 알고리즘
+    std::transform(s1.begin(), s1.end(),   // 1. 이구간의 요소들을
+                   s2.begin(),             // 2. 이구간의 요소와 함께
+                   s3.begin(),// 4. 함수의 결과를 여기에 담아달라
+                   foo);      // 3. 이 함수로 보내서
+
+    show(s3);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
