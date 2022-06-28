6935c85b9a5d48a656d1d8730ce8c3500fdc1582
ecourse <smkang@codenuri.co.kr>
2022-06-28 16:31:58 +0900

    Add files via upload

--- DAY2/5_함수객체3-1.cpp
+++ DAY2/5_함수객체3-1.cpp
@@ -34,7 +34,7 @@ public:
 
 	// 사용자가 편리하도록 몇가지 멤버함수 제공해 주세요
 	void reset() { bs.set(); } // 다시 1로 놓고
-	void count() { return bs.count(); } // 사용가능한 난수의 갯수
+	int count() { return bs.count(); } // 사용가능한 난수의 갯수
 };
 
 //URandom urand; 
