f58d6309b71ec61112a07fb08c2da7af09bc6fc2
ecourse <smkang@codenuri.co.kr>
2022-06-28 15:12:20 +0900

    Add files via upload

--- DAY2/3_algo4.cpp
+++ DAY2/3_algo4.cpp
@@ -20,4 +20,10 @@ int main()
 	s.reverse(); // 멤버 함수에 있다.. 알고리즘 보다 멤버함수를 사용해라.
 
 	show(s); // 10,9, 8,7,6,5,4,3,2,1
+
+	// std::reverse : 처음요소과 마지막 요소의 swap. 즉, 각 요소를 swap 해서 구현
+	//				  요소의 크기가 크다면 오버헤드가 있을수 있다.
+	// 
+	// list::reverse 멤버 함수 : Node 의 next/prev 포인터를 swap 하는 원리
+	//						sizeof(포인터) 크기의 swap 으로 구현.
 }
