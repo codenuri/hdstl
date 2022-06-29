72e0683aad04781354a51e2f4c3aa3f4a03b8e66
ecourse <smkang@codenuri.co.kr>
2022-06-29 16:49:28 +0900

    Add files via upload

--- DAY3/5_array3.cpp
+++ DAY3/5_array3.cpp
@@ -9,12 +9,20 @@ int main()
 	arr[0] = 0; // 결국 연속된 메모리 이므로 [] 연산 가능합니다.
 
 	// 아래 코드가 될지, 에러일지 예측해 보세요 - 메모리 구조(스택, 힙등 위치도)를 잘생각하세요
-	int n1 = arr.front();
-	int n2 = arr.back();
+	// => 결국 "진짜 배열"과 동일한 구조
+	int n1 = arr.front(); // ok
+	int n2 = arr.back();  // ok
 
-	arr.push_front(10);
-	arr.push_back(10);
+	// std::array 는 결국 배열 이므로 크기 변경, 삽입, 삭제 모두 안됩니다.
+	arr.push_front(10);	// error
+	arr.push_back(10);	// error
+	arr.resize(10);		// error
 
-	arr.resize(10);
-		
+	// 어차피 크기 변경도 안되는데, 진짜 배열 사용하지 왜? std::array 사용합니까 ?
+	// => 배열에 없는몇가지 편리한 멤버 함수가 있습니다.
+	int n3 = arr.size();
+
+	// vector vs array
+	// 속도 : array 승!
+	// 유연성 : vector 승!. 크기 변경, 삽입 삭제
 }
