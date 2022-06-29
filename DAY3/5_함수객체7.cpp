e04f4667b061397053c5211b8d6c181b305bbae2
ecourse <smkang@codenuri.co.kr>
2022-06-29 09:41:48 +0900

    Add files via upload

--- DAY3/5_함수객체7.cpp
+++ DAY3/5_함수객체7.cpp
@@ -5,22 +5,26 @@
 // sort 함수를 생각해 봅시다.
 
 // sort의 비교 정책은 사용자가 변경할수 있게 하면 좋습니다.
+// 그런데..
+// 아래 함수는 "비교 정책 변경" 이 가능해서 유연성이 좋지만
+// 직접 비교하는 코드에 비해서 너무 느립니다.(2중 루프안에서 함수 호출)
+// => 비교 함수는 절대 인라인 치환 될수 없습니다.(함수 포인터에 담아서 호출하므로)
 void Sort(int* x, int sz, bool(*cmp)(int, int) )
 {
 	for (int i = 0; i < sz - 1; i++)
 	{
 		for (int j = i + 1; j < sz; j++)
 		{
-		//	if (x[i] > x[j])  
+		//	if (x[i] > x[j])				// 1
 
-			if ( cmp(x[i], x[j]) == true )
+			if ( cmp(x[i], x[j]) == true )	// 2
 				std::swap(x[i], x[j]);
 		}
 	}
 }
 //------------------------------------
-bool cmp1(int a, int b) { return a > b; }
-bool cmp2(int a, int b) { return a < b; }
+inline bool cmp1(int a, int b) { return a > b; }
+inline bool cmp2(int a, int b) { return a < b; }
 
 int main()
 {
