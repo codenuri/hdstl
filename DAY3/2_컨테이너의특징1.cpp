359a708563d9aef703537177d01c738f079da128
ecourse <smkang@codenuri.co.kr>
2022-06-29 11:30:18 +0900

    Add files via upload

---
+++ DAY3/2_컨테이너의특징1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory>
+
+int main()
+{
+	// 아래 코드에서 메모리할당/해지 방법을 
+	// malloc/free 로 변경하려고 합니다.
+	// 몇군데를 변경해야 할까요 ? ( 모든 곳(4줄) 변경)
+	int* p1 = new int;
+	delete p1;
+
+	int* p2 = new int;
+	delete p2;
+
+	// STL 에는 메모리 할당기라는 개념이 있습니다.
+	std::allocator<int> ax; // int 타입의 메모리를 할당/해지하는 객체
+
+	int* p3 = ax.allocate(1); // int 한개 할당
+	ax.deallocate(p3, 1); 
+
+	int* p4 = ax.allocate(1); 
+	ax.deallocate(p4, 1);
+
+}
