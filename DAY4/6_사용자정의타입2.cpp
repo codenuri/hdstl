3302ae3e53fbf13bc8b2e84d6f4c579246f59858
ecourse <smkang@codenuri.co.kr>
2022-06-30 15:31:16 +0900

    Add files via upload

--- DAY4/6_사용자정의타입2.cpp
+++ DAY4/6_사용자정의타입2.cpp
@@ -28,8 +28,14 @@ int main()
 //	v.push_back(pt); // 이순간 pt 자체가 보관되는 것이 아니라
 				     // 힙에 새로운 객체를 만드는데, pt를 복사해서 만들게됩니다.
 
+	// 방법2. 임시객체(rvalue로) 넣기
+//	v.push_back( Point(1, 2) );
 
-	// 방법 2. 객체를 만들어서 전달하지 말고
+
+
+
+
+	// 방법 3. 객체를 만들어서 전달하지 말고
 	// 객체를 만들때 사용할 생성자 인자를 전달하는것이
 	// 효율적입니다.
 	v.emplace_back(1, 2);
@@ -40,6 +46,7 @@ int main()
 	// 결론 
 	// vector<int> v1;
 	// => 그냥 push_back 으로 넣으세요. v1.push_back(10)
+	// => emplace_back 해도 전혀 나쁘지 않습니다. 동일 성능.. 
 
 	// vector<Point> v2; 즉, 사용자 타입을 저장할때
 	// => push_xxx() 계열 함수 대신, emplace_xxx() 사용하세요
@@ -49,9 +56,22 @@ int main()
 	// push_front => emplace_front
 	// insert     => emplace
 
+	// emplace_xxx 는 C++11 부터 됩니다.
+	// => emplace_xxx 함수를 만들때는 "완벽한전달"이라는 기술이 있어야 합니다.
+	// => 이 기술이 "C++11" 에서 나왔습니다.
 
 	std::cout << "------------------" << std::endl;	
 }
+
+// emplace는 R-value를 저장하고자 하는 경우에만 유용하겠네요?  
+// => 네.. 객체를 만든후에 다른 용도로 사용후 저장할때는 
+// => push_xx 로하세요
+// // 
+// 그런데 요즘은 shared_ptr을 보통 많이 저장하지 않나요?
+// => vector<사용자정의타입포인터*>, vector<스마트포인터>, 
+// => push_xxx() 사용하면 됩니다.
+// => 스마트포인터 할때 다루게 됩니다.
+
 
 
 
