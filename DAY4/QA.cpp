0874bbb97367414115e581fac0c36901d1626ea0
ecourse <smkang@codenuri.co.kr>
2022-06-30 11:49:49 +0900

    Add files via upload

--- DAY4/QA.cpp
+++ DAY4/QA.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <algorithm>
 
+int x[5] = { 1,2,3,4,5 };
+int* fn() { return x; } // 배열의 시작 주소 반환
+
 int main()
 {
 //	std::vector<int> c = { 1,2,3,4,5,6,7,8,9,10 };
@@ -23,5 +26,16 @@ int main()
 	int n = std::distance(c.begin(), ret); 
 
 	std::cout << n << std::endl;
+
+	// 2번째 요소를 가리키는 반복자를 꺼내고 싶다.
+	auto p1 = ++(c.begin());		// 1. ok.. 
+	auto p2 = std::next(c.begin()); // 2. 이렇게 해도 됩니다.
+
+
+	// 1이면 충분한거 같은데.. 왜.. 2를 하나요..??
+	// => 아주 드물게, 1이 안될때가 있습니다.
+	auto p3 = ++(fn()); // error. 반환된 주소값은 rvalue 입니다.
+						// rvalue 는 ++ 안됩니다.
+	auto p4 = std::next(fn()); // ok.. 
 }
 
