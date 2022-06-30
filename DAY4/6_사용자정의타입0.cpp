027f29be73b7bc6370df08477dfbce4c13ff29c7
ecourse <smkang@codenuri.co.kr>
2022-06-30 14:49:26 +0900

    Add files via upload

--- DAY4/6_사용자정의타입0.cpp
+++ DAY4/6_사용자정의타입0.cpp
@@ -17,10 +17,29 @@ int main()
 
 	// user define type 을 저장하는 컨테이너
 	// 아래 2개 꼭 구별하세요
-	std::vector<Point> v2 = { Point(1,1), Point(2,2) };
-	std::vector<Point*> v3 = { new Point(1,1), new Point(2,2) };
+	// std::vector<Point>  : Point 객체를 보관
+	// std::vector<Point*> : Point 객체의 주소를 보관
 
+	std::vector<Point>  v2 = { Point(1,1), Point(2,2) };
+//	std::vector<Point*> v3 = { new Point(1,1), new Point(2,2) };
+	std::vector<Point*> v3(2);
+
+	v3[0] = new Point(1, 1);
+	v3[1] = new Point(2, 2);
+
+//	Point pt;
+//	v3[1] = &pt; // 이렇게도 가능합니다.
+
+	// 핵심 : 사용자가 new 한 객체는 사용자가 delete 해야 합니다.
+	// v3의 모든 객체를 사용자가 new로 만들었다면 v3 파괴전에 모두 delete
+	// 해야 합니다.
+	for (auto p : v3)
+		delete p;
 }
+
+
+
+
 
 
 
