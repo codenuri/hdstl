54c805b39164f80bbdd50965ef8d9866fe4596cc
ecourse <smkang@codenuri.co.kr>
2022-06-30 10:47:19 +0900

    Add files via upload

--- DAY4/2_유틸리티3-2.cpp
+++ DAY4/2_유틸리티3-2.cpp
@@ -8,11 +8,21 @@ int main()
 	std::ostringstream oss; // 메모리 출력 스트림 
 	oss << "hello";
 
+
 	std::istringstream iss("to be or not to be"); 
 
 	std::string s;
 
 //	std::cin >> s;	// 표준 입력에서 한 단어 입력
-	iss >> s;		// iss 의 내부 문자열에서 한단어 입니다.
+//	iss >> s;		// iss 의 내부 문자열에서 한단어 입니다.
 
+	// 아래 코드의 의미를 생각해 보세요
+	while (iss >> s)
+		std::cout << s << std::endl;
 }
+
+
+
+
+
+
