4f1840e80b9a17d0695521c6195327315f99f3e3
ecourse <smkang@codenuri.co.kr>
2022-07-01 16:13:34 +0900

    Add files via upload

---
+++ DAY5/6_ranges2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<typename T> 
+class take_view
+{
+	T& r;
+	int count;
+public:
+	take_view(T& c, int n) : r(c), count(n) {}
+
+	auto begin() { return r.begin(); }
+	auto end()   { return r.begin() + count; }
+};
+   
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
+
+	take_view tv(v, 3); // C++17 부터는 템플릿 인자 생략가능
+
+//	for (auto e : v)
+	for (auto e : tv)
+	{
+		std::cout << e << std::endl;
+	}
+
+
+
+}
+
